46743a87bfc7bb669208c39574421a7ec8d58434
wooldridge12 <wooldridgemikey@gmail.com>
2021-11-01 15:29:31 -0400

    MooseAks function is a boolean.

--- Program.cs
+++ Program.cs
@@ -15,8 +15,22 @@ Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
 Console.WriteLine();
 
+//This is where we are invoking our Moose Functions!
+
+//What the moose says for intorpolation. This is our MooseSays Functions
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
+
+// MooseAsks Functions
+bool isTrue = MooseAsks("Is Canada real?");
+if (isTrue) 
+{
+    MooseSays("YAAAAAY!!!!!");
+}
+else
+{
+    MooseSays("Oh Well atleast I can dream of it being real!!!");
+}
 
 
 void MooseSays(string message)
@@ -52,4 +66,27 @@ void MooseSays(string message)
                       \ \'._  ` '_.'
                        `^^` `^^^`
     ");
+}
+
+
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
